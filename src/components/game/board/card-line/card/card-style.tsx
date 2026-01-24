b6ddae7c06eddce39fffeadfaec1c97c02980306
Captain Arkeo <captainarkeo@gmail.com>
2026-01-24 21:34:12 +0100

    Feature Completed : Cards and their styles.

--- src/components/game/board/card-line/card/card-style.tsx
+++ src/components/game/board/card-line/card/card-style.tsx
@@ -1,7 +1,114 @@
 import { styled } from "@/styled-system/jsx";
 
 export const CardStyled = styled("div", {
-    base: {
+  base: {
+    display: "flex",
+    height: "190px",
+    width: "150px",
+    borderColor: "black",
+    borderRadius: "20px",
+    borderWidth: "2px",
+    borderStyle: "solid",
+    position: "relative",
+    overflow: "hidden",
 
-    }
-})
+    alignItems: "flex-end",
+  },
+  variants: {
+    flipped: {
+      true: {
+        alignItems: "center",
+        justifyContent: "center",
+      },
+    },
+
+    //Only there to prepare the compound variants
+    player: {
+      1: {},
+      2: {},
+      3: {},
+      4: {},
+    },
+  },
+
+  /*Making sure colors are consistent with player choice*/
+  compoundVariants: [
+    {
+      flipped: true,
+      player: 1,
+      css: {
+        bgColor: "player1",
+      },
+    },
+    {
+      flipped: true,
+      player: 2,
+      css: {
+        bgColor: "player2",
+      },
+    },
+    {
+      flipped: true,
+      player: 3,
+      css: {
+        bgColor: "player3",
+      },
+    },
+    {
+      flipped: true,
+      player: 4,
+      css: {
+        bgColor: "player4",
+      },
+    },
+  ],
+});
+
+export const NameStyled = styled("span", {
+  base: {
+    fontWeight: "bold",
+    width: "100%",
+    textAlign: "center",
+    color: "white",
+    zIndex: "1",
+
+    fontSize: "18px",
+    marginBottom: "23px",
+  },
+  variants: {
+    player: {
+      1: {
+        bgColor: "player1",
+      },
+      2: {
+        bgColor: "player2",
+      },
+      3: {
+        bgColor: "player3",
+      },
+      4: {
+        bgColor: "player4",
+      },
+    },
+
+    //When flipped, the text becomes "?"
+    flipped: {
+      true: {
+        fontSize: "128px",
+        marginBottom: "0px",
+      },
+    },
+  },
+});
+
+export const CardImageStyled = styled("img", {
+  base: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    top: "0",
+    left: "0",
+    right: "0",
+    bottom: "0",
+  },
+});
--- src/components/game/board/card-line/card/card.tsx
+++ src/components/game/board/card-line/card/card.tsx
@@ -1,8 +1,43 @@
-import { css } from "@/styled-system/css";
-import { CardStyled } from "./card-style";
+import { Players } from "@/src/types";
+import { CardImageStyled, CardStyled, NameStyled } from "./card-style";
 
-export const Card: React.FC = () => {
+export interface CardProps {
+  player: Players;
+  flipped: boolean;
+  name?: string;
+  imageUrl?: string;
+  className?: string;
+}
+
+export const Card: React.FC<CardProps> = ({
+  player,
+  flipped,
+  name,
+  imageUrl,
+  className,
+}) => {
   return (
-    <></>
+    <>
+      {/*When upright, the card shows the image and character's name. When
+      flipped, we see the back of the card instead, hiding the character*/}
+
+      {flipped ? (
+        <CardStyled className={className} player={player} flipped={true}>
+          <NameStyled flipped={true}>?</NameStyled>
+        </CardStyled>
+      ) : (
+        <CardStyled className={className}>
+          {name ? <NameStyled player={player}>{name}</NameStyled> : <></>}
+          {imageUrl ? (
+            <CardImageStyled
+              src={imageUrl}
+              alt={`Picture of ${name ?? "an unknown character"}`}
+            />
+          ) : (
+            <></>
+          )}
+        </CardStyled>
+      )}
+    </>
   );
 };
