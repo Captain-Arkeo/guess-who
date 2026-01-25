62840df31eac330923716768df5643c3636e38d5
Captain Arkeo <captainarkeo@gmail.com>
2026-01-26 00:47:03 +0100

    Prepared mainPage using base important features : customized characters and an interactable board of cards

---
+++ src/containers/pages/main-page/main-page-style.tsx
@@ -0,0 +1,13 @@
+import { styled } from "@/styled-system/jsx";
+
+export const MainPageStyled = styled("div", {
+  base: {},
+});
+
+export const CustomizationSectionStyled = styled("div", {
+  base: {
+    display: "flex",
+    justifyContent: "space-around",
+    marginBottom: "50px",
+  },
+});
--- src/containers/pages/main-page/main-page.tsx
+++ src/containers/pages/main-page/main-page.tsx
@@ -1,3 +1,57 @@
+"use client";
+import { Board } from "@/src/components";
+import { Character, Players } from "@/src/types";
+import { useState } from "react";
+import { CharacterForm } from "../../customization";
+import { CustomizationSectionStyled, MainPageStyled } from "./main-page-style";
+
 export const MainPage: React.FC = () => {
-  return <div>This page sure is empty !</div>;
+  const [nbRows, setNbRows] = useState<number>(4);
+  const [nbColumns, setNbColumns] = useState<number>(7);
+
+  const [nbEmptyCards, setNbEmptyCards] = useState<number>(nbRows * nbColumns);
+
+  const [characters, setCharacters] = useState<Character[]>(
+    Array.from({ length: nbRows * nbColumns }, () => ({
+      id: crypto.randomUUID(),
+    })),
+  );
+
+  //Adds a new character to the characters array
+  function addNewCharacter(newName: string, newImageUrl: string): void {
+    if (nbEmptyCards == 0) {
+      return;
+    }
+
+    const updatedCharacters = characters.slice();
+
+    updatedCharacters.map((previousCharacter, index) => {
+      if (updatedCharacters.length - nbEmptyCards == index) {
+        updatedCharacters[index] = {
+          //Keep the generated id but add the new character
+          id: previousCharacter.id,
+          name: newName != "" ? newName : undefined,
+          imageUrl: newImageUrl != "" ? newImageUrl : undefined,
+        };
+      }
+    });
+
+    setNbEmptyCards(nbEmptyCards - 1);
+    setCharacters(updatedCharacters);
+  }
+
+  return (
+    <MainPageStyled>
+      <CustomizationSectionStyled>
+        <CharacterForm onFormSubmit={addNewCharacter} />
+      </CustomizationSectionStyled>
+
+      <Board
+        characters={characters}
+        player={Players.P2}
+        nbRows={nbRows}
+        nbColumns={nbColumns}
+      />
+    </MainPageStyled>
+  );
 };
