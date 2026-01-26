b5dfad07953bd96e59581d13b77503b0249b3c91
Captain Arkeo <captainarkeo@gmail.com>
2026-01-26 21:43:46 +0100

    feature: Added a new secondary type for Characters, to allow characters without id

--- src/types/character.ts
+++ src/types/character.ts
@@ -1,5 +1,8 @@
 export type Character = {
-    id: string,
-    name?: string,
-    imageUrl?: string,
-}
+  id: string;
+  name?: string;
+  imageUrl?: string;
+};
+
+//Since user inputs (either from a list or a character added by hand) don't contain a generated id, this type is gonna used for those
+export type CharacterWithNoId = Omit<Character, "id">;
