0b2f41fa6629c7451aa90b59919f61269a79972e
Captain Arkeo <captainarkeo@gmail.com>
2026-03-12 01:52:18 +0100

    feature: modified Button props

--- src/components/common/button/button.tsx
+++ src/components/common/button/button.tsx
@@ -2,13 +2,14 @@ import { ButtonStyled } from "./button-style";
 
 export type ButtonProps = {
   type: "submit" | "button";
-  label: string;
+  label?: string;
+  onClick?: () => void;
   className?: string;
 };
 
-export const Button: React.FC<ButtonProps> = ({ type, label, className }) => {
+export const Button: React.FC<ButtonProps> = ({ type, label, onClick, className }) => {
   return (
-    <ButtonStyled type={type} className={className}>
+    <ButtonStyled type={type} className={className} onClick={onClick}>
       {label}
     </ButtonStyled>
   );
