a49696905777e7419d0925a97d9402ddaf3210e9
Captain Arkeo <captainarkeo@gmail.com>
2026-03-30 23:56:07 +0200

    refactor: added the constants folder for an easier use of default and min/max values

---
+++ src/constants/board-constants.ts
@@ -0,0 +1,11 @@
+export const MIN_ROWS = 1;
+
+export const MAX_ROWS = 100;
+
+export const DEFAULT_ROWS = 4;
+
+export const MIN_COLUMNS = 3;
+
+export const MAX_COLUMNS = 7;
+
+export const DEFAULT_COLUMNS = 7;
---
+++ src/constants/player-constants.ts
@@ -0,0 +1,3 @@
+import { Players } from "../types";
+
+export const DEFAULT_PLAYER : Players = Players.P1;
