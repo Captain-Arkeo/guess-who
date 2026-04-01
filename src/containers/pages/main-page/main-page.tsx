90369da5b1d89f2854f6632fafdfbdcad6d0c367
Captain Arkeo <captainarkeo@gmail.com>
2026-04-01 16:07:03 +0200

    feature: added miscellaneous choices to main page + changed default values to use constants instead

--- src/containers/pages/main-page/main-page.tsx
+++ src/containers/pages/main-page/main-page.tsx
@@ -1,18 +1,23 @@
 "use client";
-import { Board } from "@/src/components";
+import { Board, RangeInput } from "@/src/components";
 import { Character, Players } from "@/src/types";
 import { useEffect, useState } from "react";
 import { CharacterForm, CharacterListForm } from "../../customization";
 import { CustomizationSectionStyled, MainPageStyled } from "./main-page-style";
 import { useBoard } from "@/src/hooks";
+import { PlayerContext, SizeContext } from "@/src/context";
+import { DEFAULT_COLUMNS, DEFAULT_PLAYER, DEFAULT_ROWS } from "@/src/constants";
+import { MiscellaneousChoices } from "../../customization/miscellaneous-choices";
 
 export const MainPage: React.FC = () => {
-  const [nbRows, setNbRows] = useState<number>(4);
-  const [nbColumns, setNbColumns] = useState<number>(7);
-  const [player, setPlayer] = useState<Players>(Players.P1);
+  const [nbRows, setNbRows] = useState<number>(DEFAULT_ROWS);
+  const [nbColumns, setNbColumns] = useState<number>(DEFAULT_COLUMNS);
+  const [player, setPlayer] = useState<Players>(DEFAULT_PLAYER);
 
-  const { characters, nbEmptyCards, resetBoard, addNewCharacters } = useBoard(nbRows, nbColumns);
-
+  const { characters, nbEmptyCards, resetBoard, addNewCharacters } = useBoard(
+    nbRows,
+    nbColumns,
+  );
 
   //Whenever the number of rows or columns changes, reset the board
   useEffect(() => {
@@ -22,8 +27,17 @@ export const MainPage: React.FC = () => {
   return (
     <MainPageStyled>
       <CustomizationSectionStyled>
-        <CharacterListForm characters={characters} onFormSubmit={addNewCharacters}></CharacterListForm>
+        <CharacterListForm
+          characters={characters}
+          onFormSubmit={addNewCharacters}
+        />
         <CharacterForm onFormSubmit={addNewCharacters} />
+        {/*While I can put the values immediately since they share the same name, I believe it is easier to track in this way */}
+        <SizeContext value={{ nbRows: nbRows, setNbRows: setNbRows, nbColumns: nbColumns, setNbColumns: setNbColumns }}>
+          <PlayerContext value={{ player: player, setPlayer: setPlayer }}>
+            <MiscellaneousChoices></MiscellaneousChoices>
+          </PlayerContext>
+        </SizeContext>
       </CustomizationSectionStyled>
 
       <Board
