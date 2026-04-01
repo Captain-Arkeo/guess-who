2af474ec07fa99f8e20c0243658c209f030f60b9
Captain Arkeo <captainarkeo@gmail.com>
2026-04-01 16:05:26 +0200

    feature: created container for miscellaneous choices (color, size)

---
+++ src/containers/customization/miscellaneous-choices/miscellaneous-choices-style.tsx
@@ -0,0 +1,8 @@
+import { CustomizationElement, Form } from "@/src/components";
+import { styled } from "@/styled-system/jsx";
+
+export const MiscellaneousChoicesWrapperStyled = styled(CustomizationElement, {
+  base: {
+    gap: "10px",
+  },
+});
