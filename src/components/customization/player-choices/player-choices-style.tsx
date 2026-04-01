0760808b02c231033f6f64d3e37b7999051e2b6a
Captain Arkeo <captainarkeo@gmail.com>
2026-04-01 16:03:31 +0200

    feature: component to choose players/colors created (needs to be styled correctly)

--- src/components/customization/player-choices/player-choices-style.tsx
+++ src/components/customization/player-choices/player-choices-style.tsx
@@ -4,8 +4,12 @@ export const PlayerChoicesStyled = styled("div", {
   base: {
     display: "flex",
     flexDirection: "row",
+<<<<<<< HEAD
     justifyContent: "center",
     gap: "10px",
+=======
+    justifyContent: "space-around",
+>>>>>>> b10661d (feature: component to choose players/colors created (needs to be styled correctly))
     width: "80%",
   },
 });
